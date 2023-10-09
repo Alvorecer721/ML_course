9eb025fd86f2b26433971c6b54d37bcd37647b45
Alvorecer721 <1535051507@qq.com>
2023-10-09 21:56:57 +0200

    push ex04 progress

--- labs/ex04/template/least_squares.py
+++ labs/ex04/template/least_squares.py
@@ -22,9 +22,10 @@ def least_squares(y, tx):
     >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
     (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # least squares: TODO
-    # returns optimal weights, MSE
-    # ***************************************************
-    raise NotImplementedError
+    A = tx.T @ tx
+    b = tx.T @ y
+    optimal_weight = np.linalg.solve(A, b)
+
+    e = y - tx.dot(optimal_weight)
+    mse = (e.T.dot(e)) / (2*len(y))
+    return optimal_weight, mse
--- labs/ex04/template/ridge_regression.py
+++ labs/ex04/template/ridge_regression.py
@@ -23,8 +23,7 @@ def ridge_regression(y, tx, lambda_):
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
     array([0.03947092, 0.00319628])
     """
-    # ***************************************************
-    # COPY YOUR CODE FROM EX03 HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+    A = tx.T @ tx + lambda_ * 2 * len(y) * np.identity(tx.shape[1])
+    b = tx.T @ y
+    w = np.linalg.solve(A, b)
+    return w
